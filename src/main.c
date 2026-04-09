15abc94b6d5b65e8da43be1dd9868f089d8a8ea3
FrancescoPaoloL <francescopaololezza@gmail.com>
2026-04-09 16:55:19 +0200

    feat: add entry point

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "../include/pngscanner.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s input.png [output.zip]\n", argv[0]);
+        return 1;
+    }
+
+    const char *input_png   = argv[1];
+    const char *zip_out     = argc >= 3 ? argv[2] : NULL;
+
+    png_scan(input_png, zip_out);
+
+    return 0;
+}
+
